9bea3878897613db74971663a5ff614e1ed856e0
Mingjie Jiang <jiang@mingjie.info>
2020-01-26 01:20:26 -0500

    Fix video order

--- assets/home/js/app.js
+++ assets/home/js/app.js
@@ -1,41 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
 
-  if ("IntersectionObserver" in window) {
-    var lazyVideoObserver = new IntersectionObserver(function(
-      entries,
-      observer
-    ) {
-      entries.forEach(function(video) {
-        if (video.isIntersecting) {
-          for (var source in video.target.children) {
-            var videoSource = video.target.children[source];
-            if (
-              typeof videoSource.tagName === "string" &&
-              videoSource.tagName === "SOURCE"
-            ) {
-              videoSource.src = videoSource.dataset.src;
-            }
+if ("IntersectionObserver" in window) {
+  var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(function(video) {
+      if (video.isIntersecting) {
+        for (var source in video.target.children) {
+          var videoSource = video.target.children[source];
+          if (
+            typeof videoSource.tagName === "string" &&
+            videoSource.tagName === "SOURCE"
+          ) {
+            videoSource.src = videoSource.dataset.src;
           }
-
-          video.target.load();
-          video.target.classList.remove("lazy");
-          lazyVideoObserver.unobserve(video.target);
         }
-      });
-    });
 
-    lazyVideos.forEach(function(lazyVideo) {
-      lazyVideoObserver.observe(lazyVideo);
+        video.target.load();
+        video.target.classList.remove("lazy");
+        lazyVideoObserver.unobserve(video.target);
+      }
     });
-  }
+  });
 
-  document.getElementById("landing-vid").addEventListener(
-    "loadedmetadata",
-    function() {
-      this.currentTime = 2;
-      this.style.filter = "brightness(50%) blur(5px)";
-    },
-    false
-  );
-});
+  lazyVideos.forEach(function(lazyVideo) {
+    lazyVideoObserver.observe(lazyVideo);
+  });
+}
+
+document.getElementById("landing-vid").addEventListener(
+  "loadedmetadata",
+  function() {
+    this.currentTime = 2;
+    this.style.filter = "brightness(50%) blur(5px)";
+  },
+  false
+);
