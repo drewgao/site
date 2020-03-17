844b683e4547f95f5dc9af36348cb4035d62246c
Mingjie Jiang <jiang@mingjie.info>
2020-03-16 22:42:34 -0400

    Sanitize string

--- assets/grant/js/app.js
+++ assets/grant/js/app.js
@@ -1,9 +1,17 @@
 const urlParams = new URLSearchParams(window.location.search)
-const customEvent = urlParams.get("event")
+const customEvent = sanitize(urlParams.get('event'))
 
 if (customEvent) {
-    $("#page-title").addClass("custom")
-    $("#page-title").html(`Travel to <strong>${customEvent}</strong> For Free`)
-    $("#page-subtitle").html(`<strong>Travel Grant</strong> is a scholarship awarded to <strong>passionate</strong> high school hackers <strong>in need</strong>,
+  $('#page-title').addClass('custom')
+  $('#page-title').html(`Travel to <strong>${customEvent}</strong> For Free`)
+  $('#page-subtitle')
+    .html(`<strong>Travel Grant</strong> is a scholarship awarded to <strong>passionate</strong> high school hackers <strong>in need</strong>,
     giving them the opportunity to attend awesome events like <strong>${customEvent}</strong>.`)
+}
+
+// Due to coronavirus, sanitize string (no XSS, as if that matters)
+function sanitize(str) {
+    var temp = document.createElement('div');
+	temp.textContent = str;
+	return temp.innerHTML;
 }
