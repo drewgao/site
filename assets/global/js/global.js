657f3a1b860991c78fc5bca11e901760fe72eaee
Mingjie Jiang <jiang@mingjie.info>
2020-03-16 23:28:36 -0400

    Enable global announcement

--- assets/global/js/global.js
+++ assets/global/js/global.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
   // Check for click events on the navbar burger icon
-  $(".navbar-burger").click(function() {
+  $('.navbar-burger').click(function() {
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-    $(".navbar-burger").toggleClass("is-active");
-    $(".navbar-menu").toggleClass("is-active");
-  });
-});
+    $('.navbar-burger').toggleClass('is-active')
+    $('.navbar-menu').toggleClass('is-active')
+  })
+})
+
+// script for announcement state saving
+const lastClosed = parseInt(localStorage.getItem('lastClosed'))
+
+if (
+  announcementId != -1 && // if there is an announcemnet
+  !(lastClosed != NaN && lastClosed >= announcementId) // if the announcement has not been closed before
+) {
+  $('#announcement').show()
+  $('#announcement').scroll()
+
+  $('#close-announcement').click(() => {
+    localStorage.setItem('lastClosed', announcementId)
+    $('#announcement').hide()
+  })
+}
